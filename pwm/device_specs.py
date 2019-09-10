7eb81f4b49a78cfbe4e3072b8f7c1f09e342535a
crystaldust <juzhenatpku@gmail.com>
2019-09-10 21:04:44 +0800

    Add more specs

--- pwm/device_specs.py
+++ pwm/device_specs.py
@@ -8,9 +8,12 @@ class DeviceType(Enum):
 
 class ESCModels(Enum):
     QuicRun_WP_16BL30 = 1
+    HOBBYWING_WP_1625 = 2
+
 
 class ServoModels(Enum):
     D115F = 1001
+    E6001 = 1002
 
 specs = {
     ServoModels.D115F: {
@@ -19,10 +22,22 @@ specs = {
         'PWM_mid': 1538,
         'PWM_max': 1742
     },
+    ServoModels.E6001: {
+        'Frequency': 62,
+        'PWM_min': 985, # Actual output: 994
+        'PWM_mid': 1470, # Actual output: 1484
+        'PWM_max': 1990  # Actual output: 2015
+    },
     ESCModels.QuicRun_WP_16BL30: {
         'Frequency': 75,
         'PWM_min': 1220,
         'PWM_mid': 1520,
         'PWM_max': 1820
+    },
+    ESCModels.HOBBYWING_WP_1625: {
+        'Frequency': 62,
+        'PWM_min': 1000, # Actual output: 1012
+        'PWM_mid': 1465, # Actual output: 1483
+        'PWM_max': 1964  # Actual output: 1982
     }
 }
